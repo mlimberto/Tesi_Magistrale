8b325cd18e309a319d3658149d2f560c131bd4eb
mlimberto <m.limberto@gmail.com>
2015-10-02 12:50:05 +0200

    Aggiunti multiple trials

--- FreeFem/run_test.py
+++ FreeFem/run_test.py
@@ -97,6 +97,57 @@ def p1_vs_p2(gstep,beta,Nmax) :
 	# end of function
 
 
+
+def run_multiple_trials(gsteps,betas,Nmaxs,FE_types,TEST_NAME) : 
+
+	# Create output folder
+	call(["mkdir","-p","export/trials/"+TEST_NAME])
+
+	# Run Freefem script
+	for (gstep,beta,Nmax,FE_type) in itertools.product(gsteps,betas,Nmaxs,FE_types) : 
+		test_name = str(Nmax)+"_"+str(beta)+"_"+str(gstep)+"_"+FE_type
+		# Write param file
+		write_param_file(gstep,beta,Nmax,FE_type,False)
+		run_freefem()
+		call(["mv","export/J.txt","export/trials/"+TEST_NAME+"/"+test_name+"_J.txt"])
+		call(["mv","export/tJ.txt","export/trials/"+TEST_NAME+"/"+test_name+"_tJ.txt"])
+		
+	# end of function
+
+def plot_trials_result(gsteps,betas,Nmaxs,FE_types,TEST_NAME) : 
+	# Create output folder
+	call(["mkdir","-p","export/trials/"+TEST_NAME+"/images"])
+
+	# Write visualize.conf file
+	f = open("visualize.conf",'w')
+	f.write("#GNUPLOT script written by Python testing script\n\n")
+	f.write("set terminal postscript eps enhanced color\n")
+	f.write("set autoscale\n")
+
+	# Plot all Js
+	f.write("set xlabel 'Iteration'\nset ylabel 'Cost functional'\nset style data lines\n")
+	f.write("\n\n")
+	for (gstep,beta,Nmax,FE_type) in itertools.product(gsteps,betas,Nmaxs,FE_types) : 
+		test_name = str(Nmax)+"_"+str(beta)+"_"+str(gstep)+"_"+FE_type
+		f.write("set output 'export/trials/"+TEST_NAME+"/images/"+test_name+"_J'\n")
+		f.write("plot 'export/trials/"+TEST_NAME+"/"+test_name+"_J.txt' lw 3 lc 'blue' title 'Cost Functional'\n")
+
+
+
+
+	f.close()
+
+	# Run gnuplot and remove conf file
+	call(["gnuplot","visualize.conf"])
+	call(["rm","visualize.conf"])
+
+	# end of function
+
+
 if __name__ == '__main__' : 
 
-	p1_vs_p2(0.004,0.001,20)
+	# p1_vs_p2(0.004,0.001,20)
+
+	# run_multiple_trials([0.004, 0.008],[0.001],[5],["P1"],"Test_Python")
+
+	plot_trials_result([0.004, 0.008],[0.001],[5],["P1"],"Test_Python")
