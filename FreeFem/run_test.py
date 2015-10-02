4c4a47a5874f8e0c23087d3755a7fce00404fce5
mlimberto <m.limberto@gmail.com>
2015-10-02 12:12:59 +0200

    Aggiunto test P1 vs P2

--- FreeFem/run_test.py
+++ FreeFem/run_test.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 from subprocess import call
-import os
+import os,itertools
 
 print("Running the Freefem simulation on various configurations")
 
@@ -40,9 +40,63 @@ def run_freefem() :
 
 	# end of function
 
+def plot_J() : 
+	# Write gnuplot config file
+	f = open("visualize.conf",'w')
+	f.write("#GNUPLOT script written by Python testing script\n\n")
+	f.write("set terminal postscript eps enhanced color\n")
+	f.write("set autoscale\nset xlabel 'Iteration'\nset ylabel 'Cost functional'\nset style data lines\n")
+
+	f.write("set output 'export/images/J'\n")
+	f.write("plot 'export/J.txt' lw 3 lc 'blue' title 'Cost Functional'\n")
+
+	f.write("set output 'export/images/tJ'\n")
+	f.write("plot 'export/tJ.txt' lw 3 lc 'blue' title 'true Cost Functional'\n")
+
+	f.close()
+
+	# Run gnuplot and then remove the config file
+	call(["gnuplot","visualize.conf"])
+	call(["rm","visualize.conf"])
+
+	# end of function
+
+def p1_vs_p2(gstep,beta,Nmax) : 
+
+	print "P1 vs P2 test "
+	test_name = str(Nmax)+"_"+str(beta)+"_"+str(gstep)
+	call(["mkdir","-p","export/trials/"+test_name])
+	# Run the P1 case
+	print "Running the P1 case... "
+	write_param_file(gstep,beta,Nmax,"P1",False)
+	run_freefem()
+	call(["mv","export/J.txt","export/trials/"+test_name+"/J_P1.txt"])
+
+	# Run the P2 case
+	print "Running the P2 case... "
+	write_param_file(gstep,beta,Nmax,"P2",False)
+	run_freefem()
+	call(["mv","export/J.txt","export/trials/"+test_name+"/J_P2.txt"])
+	
+	# Plot output data
+	f = open("visualize.conf",'w')
+	f.write("#GNUPLOT script written by Python testing script\n\n")
+	f.write("set terminal postscript eps enhanced color\n")
+	f.write("set autoscale\nset xlabel 'Iteration'\nset ylabel 'Cost functional'\nset style data lines\n")
+
+	f.write("set output 'export/trials/"+test_name+"/P1vsP2'\n")
+	f.write("plot 'export/trials/"+test_name+"/J_P1.txt' lw 3 lc 'blue' title 'Cost Functional P1' , ")
+	f.write("'export/trials/"+test_name+"/J_P2.txt' lw 3 lc 'red' title 'Cost Functional P2 \n")
+
+	f.close()
+
+	# Run gnuplot and then remove the config file
+	call(["gnuplot","visualize.conf"])
+	call(["rm","visualize.conf"])
+
+	# end of function
+
 
 if __name__ == '__main__' : 
 
-	write_param_file(0.004,0.001,15,"P1",False);
-
-	run_freefem()
+	p1_vs_p2(0.004,0.001,20)
