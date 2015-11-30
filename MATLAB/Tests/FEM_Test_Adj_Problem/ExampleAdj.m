fc8457864fd8effbb0b760121ec9407a790a3a56
mlimberto <m.limberto@gmail.com>
2015-11-30 10:53:29 +0100

    Added solution to adjoint problem

--- MATLAB/Tests/FEM_Test_Adj_Problem/ExampleAdj.m
+++ MATLAB/Tests/FEM_Test_Adj_Problem/ExampleAdj.m
@@ -322,13 +322,74 @@ A_adj = A' ;
 
 F_adj = zeros(size(A_adj,1),1);
 
-%% Apply boundary conditions
+%% Apply Neumann boundary conditions
+
+[csi,wi]       =  xwgl(FE_SPACE.quad_order, 0, 1);
+[phi]          =  fem_basis2D(FE_SPACE.fem, csi, 0*csi, 1);
+eta            =  1 - csi;
+nqn            =  length(csi);
+    
+nof         = length(MESH.Neumann_side);
+nbn         = MESH.numBoundaryDof;
+    
+Rrows       = zeros(nbn*nof,1);
+Rcoef       = Rrows;
+    
+xlt = zeros(nof,nqn); ylt = xlt;
+coord_ref = [eta; csi];
+
+for j = 1 : 2
+    dof = MESH.boundaries(j,MESH.Neumann_side);
+    vtemp = MESH.vertices(1,dof);
+    xlt = xlt + vtemp'*coord_ref(j,:);
+    vtemp = MESH.vertices(2,dof);
+    ylt = ylt + vtemp'*coord_ref(j,:);
+end
+
+dataNeu = scatteredInterpolant( MESH.nodes(1,:)' , MESH.nodes(2,:)' , zd -u ) ; % verify the sign!
+
+u_Neumann = dataNeu(xlt,ylt);
+one       = ones(nof,nqn);
+u_Neumann = u_Neumann.*one;
+
+x    =  MESH.vertices(1,MESH.boundaries(1:2, MESH.Neumann_side));
+y    =  MESH.vertices(2,MESH.boundaries(1:2, MESH.Neumann_side));
+   
+side_length = sqrt((x(2:2:end)-x(1:2:end-1)).^2+(y(2:2:end)-y(1:2:end-1)).^2);
+    
+for l = 1 : nof
+    face = MESH.Neumann_side(l);
+        
+    u_Neumann_loc  = u_Neumann(l,:).*wi;
+    u_Neumann_loc  = u_Neumann_loc(1,:)';
+      
+    Rrows(1+(l-1)*nbn:l*nbn)    = MESH.boundaries(1:nbn,face);
+    Rcoef(1+(l-1)*nbn:l*nbn)    = side_length(l)*phi*u_Neumann_loc;
+end
+
+F_adj = F_adj + sparse(Rrows,1,Rcoef,MESH.numNodes,1);
 
 
+%% Impose zero-mean condition and solve 
 
+A_total = [ A_adj , B ; B' , 0 ] ; 
+F_total = [ F_adj ; 0 ] ; 
 
+fprintf('\n Solve Au = f ... ');
+t_solve = tic;
+p                         = zeros(MESH.numNodes,1);
+p_total = A_total \ F_total ; 
+p(MESH.internal_dof)      = p_total(1 : end -1 ) ;
+t_solve = toc(t_solve);
+fprintf('done in %3.3f s \n', t_solve);
 
+%% Visualize solution 
 
-
+figure
+pdeplot(MESH.vertices,[],MESH.elements(1:3,:),'xydata',p(1:MESH.numVertices),'xystyle','interp',...
+       'zdata',p(1:MESH.numVertices),'zstyle','continuous',...
+       'colorbar','on', 'mesh' , 'off'  );
+colormap(jet); lighting phong ; title('Solution of the adjoint problem p') 
+% axis equal
 
 
