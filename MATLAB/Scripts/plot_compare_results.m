cf8e522cee2b49d9d3f07aab4357fb6b26e21d1a
mlimberto <m.limberto@gmail.com>
2016-02-17 14:36:00 +0100

    Added plotting utility

---
+++ MATLAB/Scripts/plot_compare_results.m
@@ -0,0 +1,52 @@
+function h = plot_compare_results( c1 , c2 , c3 ) 
+
+ax1 = gca( c1 ) ; 
+ax2 = gca( c2 ) ; 
+ax3 = gca( c3 ) ; 
+
+h = figure; %create new figure
+
+temp = h.Position ;
+temp(3) = 812 ;
+temp(4) = 413 ;
+h.Position = temp ;
+
+
+s1 = subplot(1,3,1); %create and get handle to the subplot axes
+s2 = subplot(1,3,2);
+s3 = subplot(1,3,3);
+
+fig1 = get(ax1,'children'); %get handle to all the children in the figure
+copyobj(fig1,s1); %copy children to new parent axes i.e. the subplot axes
+s1.XLim = [-4 4]
+s1.YLim = [-4 7]
+% s1.Position = [0.05 0.1 0.25 0.8]
+% axis equal
+s1.Position = [0.05 0.1 0.25 0.8]
+
+fig2 = get(ax2,'children');
+copyobj(fig2,s2);
+s2.XLim = [-4 4]
+s2.YLim = [-4 7] ;
+s2.Position = [0.35 0.1 0.25 0.8]
+% axis equal
+
+fig3 = get(ax3,'children');
+copyobj(fig3,s3);
+s3.XLim = [-4 4]
+s3.YLim = [-4 7] ;
+s3.Position = [0.65 0.1 0.25 0.8]
+% axis equal
+
+colormap jet
+
+colorbar('Position', [0.92 0.1 0.03 0.8])
+
+% Do not display axis
+axis(s1,'off')
+axis(s2,'off')
+axis(s3,'off')
+
+
+end
+
---
+++ MATLAB/Scripts/report_plots.m
@@ -0,0 +1,119 @@
+%%  Add path to libraries
+addpath('../FEM_library/')
+addpath('../Inverse_Problem/')
+addpath('../Inverse_Problem/Examples/')
+
+%% Plot target %% Import mesh
+
+addpath('../Mesh/Ovetto_Circondato/') ; % add path to mesh geometry specification function
+
+[vertices,boundaries,elements] = initmesh('ovetto_circondato_sens' , ...
+                                          'Jiggle','minimum','Hgrad',1.9,'Hmax',1000) ;
+
+% Refine only the heart subdomain
+[vertices,boundaries,elements] = refinemesh('ovetto_circondato_sens' , vertices , ... 
+                                            boundaries, elements , [1 ]) ;                                    
+
+% Refine both the heart and the torso                                        
+[vertices,boundaries,elements] = refinemesh('ovetto_circondato_sens' , vertices , ... 
+                                            boundaries, elements , [1 2 ]) ;                                         
+  
+% Reinitialize the boundary indexes
+for i = 1:4 
+    boundaries( 5 , find( boundaries(5,:) == i)  ) = 3 ;  
+end
+for i = 5:12
+    boundaries( 5 , find( boundaries(5,:) == i)  ) = 5 ;
+end
+for i = 13:26
+    boundaries( 5 , find( boundaries(5,:) == i)  ) = 13 ;
+end                                        
+
+% Add flags to identify mesh elements (in accordance with .geo file)
+DATA.FLAG_HEART_REGION = 1 ;
+DATA.FLAG_TORSO_REGION = 2 ;
+DATA.FLAG_HEART_OUTER_BOUNDARY = 3 ;
+DATA.FLAG_HEART_INNER_BOUNDARY = 5 ;
+DATA.FLAG_TORSO_BOUNDARY_NEU = 13 ;
+
+
+% Fill MESH data structure
+MESH.vertices    = vertices;
+MESH.boundaries  = boundaries;
+MESH.elements  = elements;
+MESH.numVertices = size(vertices,2);
+
+MESH.nodes = vertices;
+
+% Update Mesh data with BC information and geometrical maps
+[numElemDof,numBoundaryDof]  = select2D(fem);
+MESH.numNodes                = size(MESH.nodes,2);
+MESH.numElem                 = size(MESH.elements,2);
+MESH.numBoundaryDof          = numBoundaryDof;
+MESH.indexInnerElem = find( MESH.elements( numElemDof+ 1 ,:)== DATA.FLAG_HEART_REGION ) ; 
+MESH.innerElements = MESH.elements( : , MESH.indexInnerElem ) ;
+MESH.indexInnerNodes = unique( MESH.innerElements(1:numElemDof , :) ) ;
+MESH.innerNodes = MESH.nodes( : , MESH.indexInnerNodes ) ; 
+MESH.numInnerNodes      = size(MESH.innerNodes , 2) ;
+MESH.numInnerElem       = size(MESH.innerElements,2);
+
+% Define a target control function 
+
+tau = 0.2 ; % smoothing level
+R = 1.2 ; % radius
+
+% 1
+D = [-3.9 0]; % displacement
+w_target = circularLS( MESH.innerNodes(1,:)' , MESH.innerNodes(2,:)' , R , D ) ;
+w_target = 1 - smoothLS(w_target , tau) ;
+w_target_bar_1 = extend_with_zero( w_target , MESH ) ;
+
+% 2
+D = [0 6.8]; % displacement
+w_target = circularLS( MESH.innerNodes(1,:)' , MESH.innerNodes(2,:)' , R , D ) ;
+w_target = 1 - smoothLS(w_target , tau) ;
+w_target_bar_2 = extend_with_zero( w_target , MESH ) ;
+
+%3
+D = [-3.9 0]; % displacement
+w_target = circularLS( MESH.innerNodes(1,:)' , MESH.innerNodes(2,:)' , R , D ) ;
+w_target = 1 - smoothLS(w_target , tau) ;
+w_target_bar_3 = extend_with_zero( w_target , MESH ) ;
+
+%%
+
+c1 = figure('Visible','off') ;
+    pdeplot(MESH.vertices,[],MESH.elements(1:3,MESH.indexInnerElem),'xydata',w_target_bar_1(1:MESH.numVertices),'xystyle','interp',...
+    'zdata',w_target_bar_1(1:MESH.numVertices),'zstyle','continuous',...
+    'colorbar','off', 'mesh' , 'off' );
+    colormap(jet);
+    lighting phong
+    view([0 90])
+    axis([-4 4 -4 7])
+    axis equal
+c2 = figure('Visible','off') ;
+    pdeplot(MESH.vertices,[],MESH.elements(1:3,MESH.indexInnerElem),'xydata',w_target_bar_2(1:MESH.numVertices),'xystyle','interp',...
+    'zdata',w_target_bar_2(1:MESH.numVertices),'zstyle','continuous',...
+    'colorbar','off', 'mesh' , 'off' );
+    colormap(jet);
+    lighting phong
+    view([0 90])
+    axis([-4 4 -4 7])
+    axis equal
+c3 = figure('Visible','off') ;
+    pdeplot(MESH.vertices,[],MESH.elements(1:3,MESH.indexInnerElem),'xydata',w_target_bar_3(1:MESH.numVertices),'xystyle','interp',...
+    'zdata',w_target_bar_3(1:MESH.numVertices),'zstyle','continuous',...
+    'colorbar','off', 'mesh' , 'off' );
+    colormap(jet);
+    lighting phong
+    view([0 90])    
+    axis([-4 4 -4 7])
+    axis equal
+    
+%%    
+    
+h = plot_compare_results(c1,c2,c3);
+    
+% hp4 = get(subplot(1,3,3),'Position')
+% colorbar('Position', [hp4(1)+hp4(3)+0.02  hp4(2)  0.03  hp4(2)+hp4(3)*2.5])
+    
